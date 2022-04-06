3c532ccf938c617ffa633410f4531e0dfd3e03fd
youbin-shin <youbin5575@gmail.com>
2022-04-06 23:57:56 +0900

    Python | web scraping | part 2.11.2

--- Python/python으로 웹 스크래퍼 만들기/recruitment_website_scrap_project/incruit.py
+++ Python/python으로 웹 스크래퍼 만들기/recruitment_website_scrap_project/incruit.py
@@ -23,23 +23,23 @@ def get_last_page():
 
 def extract_job(html):
   title = html.find('span', {'class': 'accent'}).find('a', {'class': 'links'})['title']
-  company = html.find('span', {'class': 'links'}).find('a', {'class': 'strong'}).string
-  print(company)
-  
+  company = html.find('div', {'class': 'check_list_r'}).find('span', {'class': 'links'}).find('a', {'class': 'strong'})['title']
+  location = html.find_all('p', {'class': 'details_txts firstChild'})[-1].find('em').get_text()
   return {
-    'title': title
+    'title': title,
+    'company': company,
+    'location': location
   }
-
 
 def extract_jobs(last_page):
   jobs = []
   for page in range(1, last_page + 1):
     result = requests.get(f'{URL}&page={page}')
     soup = BeautifulSoup(result.text, 'html.parser')
-    results = soup.find_all('tr')
+    results = soup.find('tbody').find_all('tr')
     for result in results:
       job = extract_job(result)
-      # print(job)
+      print(job)
       jobs.append(job)
 
   return jobs
