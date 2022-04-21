d9ec941001ec79142b2e4fc476decaa0f0b86418
youbin-shin <youbin5575@gmail.com>
2022-04-21 23:38:18 +0900

    Python | web scraping | part 4.3

--- Python/python으로 웹 스크래퍼 만들기/recruitment_website_scrap_project/flask_main.py
+++ Python/python으로 웹 스크래퍼 만들기/recruitment_website_scrap_project/flask_main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask("Scrapper")
@@ -7,9 +7,9 @@ app = Flask("Scrapper")
 def home():
   return render_template("home.html")
 
-@app.route("/<username>")
-def hello(username):
-  return f"Hello {username}!"
+@app.route("/report")
+def report():
+  word = request.args.get("word")
+  return render_template("report.html", searchingBy=word)
 
 app.run() # 서버 구축: http://127.0.0.1:5000
-
